9daae46eb396f3183b2e776569743a9f96e603cb
kpk0616 <kpk0616@naver.com>
2023-04-14 17:09:49 +0900

    [Chore] 뷰 하나로 수정

---
+++ Gestalt.swiftpm/View/PagePlaygroundView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct PagePlaygroundView: View {
+    
+    /// manage user progress
+    @ObservedObject private var appState: AppState
+    
+    public init(appState: AppState) {
+        self.appState = appState
+    }
+    
+    var body: some View {
+        
+        let playgroundViewtoDraw = BasicsCourse[appState.currentPage].playgroundView
+        VStack{
+            switch playgroundViewtoDraw {
+            case .welcomePlaygroundView:
+                WelcomePlaygroundView(appState: appState)
+            case .sightPlacePlaygroundView:
+                FontsPlaygroundView(appState: appState)
+            case .hierarchyPlaygroundView:
+                HierarchyPlaygroundView(appState: appState)
+            case .appPlaygroundView:
+                AppPlaygroundView(appState: appState)
+            case .kerningPlaygroundView:
+                KerningPlaygroundView(appState: appState)
+            case .quizPlaygroundView:
+                QuizPlaygroundView(appState: appState)
+            }
+        }
+        .padding(30)
+        .padding(.top, 15)
+        .border(Color(uiColor: .secondarySystemBackground), width: 2.5)
+        .cornerRadius(10)
+//      Color(uiColor: .secondarySystemBackground)
+    }
+}
